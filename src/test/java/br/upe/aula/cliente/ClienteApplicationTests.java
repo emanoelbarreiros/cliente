3189de698640d94b8848b22d56c8fdee90fa172a
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2022-06-03 11:42:59 -0300

    first commit

---
+++ src/test/java/br/upe/aula/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.aula.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
